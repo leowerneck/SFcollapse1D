0e3b0482f9fbda63afaa41818e8717fc4e31de9e
Leonardo Werneck <leow155@gmail.com>
2020-03-11 17:56:15 -0300

    initial_condition.cpp: fixed some bugs; completed sinhspherical support

--- src/initial_condition.cpp
+++ src/initial_condition.cpp
@@ -27,9 +27,10 @@
 #include "grid.hpp"
 #include "gridfunction.hpp"
 
+using namespace std;
+
 /* Function prototypes needed by this file */
-REAL pointwise_Newton_method_Spherical( const int j, grid::parameters grid, const std::vector<REAL> Phi, const std::vector<REAL> Pi, const std::vector<REAL> a );
-REAL pointwise_Newton_method_SinhSpherical( const int j, grid::parameters grid, const std::vector<REAL> Phi, const std::vector<REAL> Pi, const std::vector<REAL> a );
+REAL pointwise_Newton_method( const int j, grid::parameters grid, const std::vector<REAL> Phi, const std::vector<REAL> Pi, const std::vector<REAL> a );
 void rescaling_of_the_lapse( grid::parameters grid, const std::vector<REAL> a, std::vector<REAL> &alpha );
 
 void initial_condition( grid::parameters grid, gridfunction &phi, gridfunction &Phi, gridfunction &Pi, gridfunction &a, gridfunction &alpha ) {
@@ -50,13 +51,20 @@ void initial_condition( grid::parameters grid, gridfunction &phi, gridfunction &
 
     if( j>0 ) {
       /* Compute a */
-      a.level_nm1[j] = pointwise_Newton_method_Spherical(j,grid,Phi.level_nm1,Pi.level_nm1,a.level_nm1);
+      a.level_nm1[j] = pointwise_Newton_method(j,grid,Phi.level_nm1,Pi.level_nm1,a.level_nm1);
 
       /* Compute auxiliary quantities */
       const REAL b = a.level_nm1[j] + a.level_nm1[j-1];
       const REAL c = a.level_nm1[j] - a.level_nm1[j-1];
-      const REAL midway_r = x[0][j] + x[0][j-1];
-      const REAL d = ( 1.0 - 0.25 * SQR(b) )/midway_r - inv_dx0 * c / b;
+#if( COORD_SYSTEM == SPHERICAL )      
+      const REAL midway_r = 0.5 * (x[0][j] + x[0][j-1]);
+#elif( COORD_SYSTEM == SINH_SPHERICAL )
+      const REAL midway_r = sinhW * tanh( inv_sinhW * 0.5 * (x[0][j] + x[0][j-1]) );
+#else
+      cerr << "ERROR! Unknown coordinate system! Check the macros.hpp file!\n";
+      exit(5);
+#endif
+      const REAL d = ( 1.0 - 0.25 * SQR(b) )/( 2.0 * midway_r ) - inv_dx0 * c / b;
 
       /* Compute alpha */
       alpha.level_nm1[j] = alpha.level_nm1[j-1]*( 1.0 - d*dx0 )/( 1.0 + d*dx0 );
